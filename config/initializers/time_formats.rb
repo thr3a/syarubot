31d88501547be392d23bb3add3b37c85f108a6fe
thr3a <12jk172@ms.dendai.ac.jp>
2016-06-04 16:05:41 +0900

    日本語化をした

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,6 @@
+Time::DATE_FORMATS[:default] = '%Y/%m/%d %H:%M'
+Time::DATE_FORMATS[:datetime] = "%Y年%m月%d日 %H時%M分"
+Time::DATE_FORMATS[:date] = "%Y/%m/%d"
+
+Date::DATE_FORMATS[:default] = "%Y年%m月%d日"
+Time::DATE_FORMATS[:time] = '%H:%M:%S'
