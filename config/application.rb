ec41d5a22fbacd2253293a5026e4242fa1ac32e4
thr3a <12jk172@ms.dendai.ac.jp>
2016-06-04 18:32:08 +0900

    サービスタイトルを定数で設定するようにした

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Myrails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.title = "My rails"
   end
 end
