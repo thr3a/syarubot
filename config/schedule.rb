5931a224ff104cf88e92268a84b5145499250ff5
thr3a <12jk172@ms.dendai.ac.jp>
2016-10-21 01:20:06 +0900

    修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,8 @@
 set :output, 'log/crontab.log' # 出力先のログファイルの指定
 set :environment, :production # ジョブの実行環境の指定
-job_type :rbenv_rake, %q!eval "$(rbenv init -)"; cd :path && :environment_variable=:environment bundle exec rake :task --silent :output!
-
+job_type :rbenv_rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\";cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
+# job_type :rbenv_rake, %q!PATH="$HOME/.rbenv/bin:$PATH"; eval "$(rbenv init -)"; cd :path && :bundle_command rake :task --silent :output!
+# job_type :rbenv_rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
 every 1.minutes do
   rbenv_rake "bot:search"
 end
