772a8139cd32c01f3c709a6efd4e6c19d4b683b7
thr3a <12jk172@ms.dendai.ac.jp>
2016-10-21 07:41:40 +0900

    MECAB_PATHの追加

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,7 @@
 set :output, 'log/crontab.log' # 出力先のログファイルの指定
 set :environment, :production # ジョブの実行環境の指定
-job_type :rbenv_rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\";cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
-# job_type :rbenv_rake, %q!PATH="$HOME/.rbenv/bin:$PATH"; eval "$(rbenv init -)"; cd :path && :bundle_command rake :task --silent :output!
-# job_type :rbenv_rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
-every 1.minutes do
+job_type :rbenv_rake, "export MECAB_PATH=/usr/local/lib/libmecab.so;export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\";cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
+
+every 23.minutes do
   rbenv_rake "bot:search"
 end
